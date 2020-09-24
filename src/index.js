286d9c08fb5816d8f6b86041b612dd3dcc56c5e6
douglasmasho <douglasmasho@gmail.com>
2020-09-24 14:55:47 +0200

    13th commit

--- src/index.js
+++ src/index.js
@@ -14,13 +14,7 @@ import thunk from "redux-thunk";
 
 // const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer,
-        
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-    )
-);
+const store = createStore(rootReducer, applyMiddleware(thunk) );
 
 
 
